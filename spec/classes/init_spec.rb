449246fe782ca33353eb131c025f56326fc8b46e
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-03-25 16:39:40 -0400

    (SIMP-2936) Allow easy modification of /etc/shells
    Users quite often will need to modify the /etc/shells content and it
    would be nice for them to be able to do this without needing to
    re-write the entire Array from scratch.
    
    To solve this, we added a shells_default parameter as well as an empty
    shells parameter which will be joined into the final file contents.
    
    Additionally, we removed the entire securetty list so that securetty is
    created as a blank file by default. This will prevent local root logins
    by default as is current best practice.
    
    If you include 'ANY_SHELL' in the securetty Array, then the
    /etc/securetty file will be removed and allow root logins from any
    source.
    
    SIMP-2936 #close

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,26 +8,55 @@ describe 'useradd' do
           facts
         end
 
-        context 'with default parameters' do
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_class('useradd') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('useradd') }
+
+        it { is_expected.to create_file('/etc/securetty').with_content(<<-EOF.gsub(/^\s+/,'').strip
+        EOF
+        ) }
+
+        it { is_expected.to create_file('/etc/shells').with_content(<<-EOF.gsub(/^\s+/,'').strip
+          /bin/sh
+          /bin/bash
+          /sbin/nologin
+          /usr/bin/sh
+          /usr/bin/bash
+          /usr/sbin/nologin
+        EOF
+        ) }
+
+        context 'when adding a securetty entry' do
+          let(:params){{
+            :securetty => ['console']
+          }}
+
           it { is_expected.to create_file('/etc/securetty').with_content(<<-EOF.gsub(/^\s+/,'').strip
             console
-            tty1
-            tty2
-            tty3
-            tty4
-            tty5
-            tty6
-            ttyS0
-            ttyS1
           EOF
           ) }
+        end
+
+        context 'when allowing from ANY_SHELL' do
+          let(:params){{
+            :securetty => ['console', 'ANY_SHELL']
+          }}
+
+          it { is_expected.to create_file('/etc/securetty').with_ensure('absent') }
+        end
+
+        context 'when adding a shell' do
+          let(:params){{
+            :shells => ['/bin/foo']
+          }}
+
           it { is_expected.to create_file('/etc/shells').with_content(<<-EOF.gsub(/^\s+/,'').strip
             /bin/sh
-            /bin/zsh
             /bin/bash
             /sbin/nologin
+            /usr/bin/sh
+            /usr/bin/bash
+            /usr/sbin/nologin
+            /bin/foo
           EOF
           ) }
         end
@@ -36,11 +65,11 @@ describe 'useradd' do
           let(:params) {{ :shells => false }}
           it { is_expected.not_to create_file('/etc/shells') }
         end
+
         context 'with securetty => false' do
           let(:params) {{ :securetty => false }}
           it { is_expected.not_to create_file('/etc/securetty') }
         end
-
       end
     end
   end
