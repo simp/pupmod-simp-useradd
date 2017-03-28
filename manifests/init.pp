31ce72beec33febc55a187059c34ffa8ac87e71f
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2017-03-28 11:19:10 -0400

    (SIMP-2936) Allow easy modification of /etc/shells (#4)
    * (SIMP-2936) Allow easy modification of /etc/shells
    
    Users quite often will need to modify the /etc/shells content and it
    would be nice for them to be able to do this without needing to
    re-write the entire Array from scratch.
    
    To solve this, we added a shells_default parameter as well as an empty
    shells parameter which will be joined into the final file contents.
    
    Additionally, we removed the entire securetty list so that securetty is
    created as a blank file by default. This will prevent local root logins
    by default as is current best practice.
    
    If you include 'ANY_SHELL' in the securetty Array, then the
    /etc/securetty file will be removed and allow root logins from any
    source.
    
    SIMP-2936 #close

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,22 +19,39 @@
 #   If true, manage `/etc/default/nss`
 #
 # @param securetty
-#   List of ttys available to log into. Set to false to disable management.
+#   List of ttys available to log into
+#
+#   * Set to false to disable management
+#   * If the Array is empty (default) then root will not be able to log into
+#     any local device
+#   * If the string 'ANY_SHELL' is found in the Array, then the
+#     ``/etc/securetty`` file will be removed and root will be able to login
+#     via any device.
+#
+# @param shells_default
+#   List of shells that will appear on the system by default
+#
+#   * These have been set to the usual suspects and users should use the
+#     ``shells`` parameter to add to the list
 #
 # @param shells
-#   List of shells available to the user to set as default. Set to false to
-#   disable management.
+#   List of shells available to the user to set as default
+#
+#   * Set to false to disable management
+#   * Will be combined with ``shells_default`` in /etc/shells
 #
 class useradd (
-  Boolean $manage_etc_profile    = true,
-  Boolean $manage_libuser_conf   = true,
-  Boolean $manage_login_defs     = true,
-  Boolean $manage_nss            = true,
-  Boolean $manage_passwd_perms   = true,
-  Boolean $manage_sysconfig_init = true,
-  Boolean $manage_useradd        = true,
-  Variant[Boolean,Array[String]]               $securetty = [ 'console','tty1','tty2','tty3','tty4','tty5','tty6','ttyS0','ttyS1' ],
-  Variant[Boolean,Array[Stdlib::AbsolutePath]] $shells = [ '/bin/sh','/bin/zsh','/bin/bash','/sbin/nologin' ]
+  Boolean                                      $manage_etc_profile    = true,
+  Boolean                                      $manage_libuser_conf   = true,
+  Boolean                                      $manage_login_defs     = true,
+  Boolean                                      $manage_nss            = true,
+  Boolean                                      $manage_passwd_perms   = true,
+  Boolean                                      $manage_sysconfig_init = true,
+  Boolean                                      $manage_useradd        = true,
+  Variant[Boolean,Array[String]]               $securetty             = [],
+
+  Array[Stdlib::AbsolutePath]                  $shells_default        = [ '/bin/sh','/bin/bash','/sbin/nologin','/usr/bin/sh','/usr/bin/bash','/usr/sbin/nologin' ],
+  Variant[Boolean,Array[Stdlib::AbsolutePath]] $shells                = []
 ) {
 
   if $manage_etc_profile    { include '::useradd::etc_profile' }
@@ -46,7 +63,15 @@ class useradd (
   if $manage_useradd        { include '::useradd::useradd' }
 
   if $securetty {
+    if 'ANY_SHELL' in $securetty {
+      $_ensure_securetty = 'absent'
+    }
+    else {
+      $_ensure_securetty = 'file'
+    }
+
     file { '/etc/securetty':
+      ensure  => $_ensure_securetty,
       owner   => 'root',
       group   => 'root',
       mode    => '0400',
@@ -54,13 +79,12 @@ class useradd (
     }
   }
 
-  if $shells {
+  if $shells and !(empty($shells_default) and empty($shells)) {
     file { '/etc/shells':
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      content => join($shells,"\n")
+      content => join(($shells_default + $shells),"\n")
     }
   }
-
 }
