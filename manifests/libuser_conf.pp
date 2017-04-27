7ca7e5738692a81733fe66e7c897ef7da7479338
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-04-26 23:06:24 -0400

    (SIMP-3112) Multiple fixes to useradd::libuser_conf
    * Fixed useradd::libuser::defaults_crypt_style to only be a String of
      the allowed values
    * Ensure that the 'modules' field is not printed if the passed Array is
      empty
    * Ensure that defaults_hash_rounds_min is less than
      defaults_hash_rounds_max and that both are within an allowed range
    * Update useradd::login_defs::pass_min_len to 15 to match the rest of
      the module updates
    
    SIMP-3112 #close

--- manifests/libuser_conf.pp
+++ manifests/libuser_conf.pp
@@ -30,34 +30,40 @@
 # @param sasl_domain
 #
 class useradd::libuser_conf (
-  Array[Useradd::LibuserModule]  $defaults_modules         = ['files','shadow'],
-  Array[Useradd::LibuserModule]  $defaults_create_modules  = ['files','shadow'],
-  Useradd::CryptStyle            $defaults_crypt_style     = ['sha512'],
-  Optional[Integer]              $defaults_hash_rounds_min = undef,
-  Optional[Integer]              $defaults_hash_rounds_max = undef,
-  Optional[Stdlib::AbsolutePath] $defaults_mailspooldir    = undef,
-  Optional[Stdlib::AbsolutePath] $defaults_moduledir       = undef,
-  Optional[Stdlib::AbsolutePath] $defaults_skeleton        = undef,
-  Stdlib::AbsolutePath           $import_login_defs        = '/etc/login.defs',
-  Stdlib::AbsolutePath           $import_default_useradd   = '/etc/default/useradd',
-  String                         $userdefaults             = "LU_USERNAME = %n\nLU_GIDNUMBER = %u",
-  String                         $groupdefaults            = 'LU_GROUPNAME = %n',
-  Optional[Stdlib::AbsolutePath] $files_directory          = undef,
-  Optional[Boolean]              $files_nonroot            = undef,
-  Optional[Stdlib::AbsolutePath] $shadow_directory         = undef,
-  Optional[Boolean]              $shadow_nonroot           = undef,
-  Optional[String]               $ldap_userbranch          = undef,
-  Optional[String]               $ldap_groupbranch         = undef,
-  Optional[String]               $ldap_server              = undef,
-  Optional[String]               $ldap_basedn              = undef,
-  Optional[String]               $ldap_binddn              = undef,
-  Optional[String]               $ldap_user                = undef,
-  Optional[String]               $ldap_password            = undef,
-  Optional[String]               $ldap_authuser            = undef,
-  Optional[String]               $ldap_bindtype            = undef,
-  Optional[String]               $sasl_appname             = undef,
-  Optional[String]               $sasl_domain              = undef
+  Array[Useradd::LibuserModule]     $defaults_modules         = ['files','shadow'],
+  Array[Useradd::LibuserModule]     $defaults_create_modules  = ['files','shadow'],
+  Useradd::CryptStyle               $defaults_crypt_style     = 'sha512',
+  Optional[Integer[1000,999999999]] $defaults_hash_rounds_min = undef,
+  Optional[Integer[1000,999999999]] $defaults_hash_rounds_max = undef,
+  Optional[Stdlib::AbsolutePath]    $defaults_mailspooldir    = undef,
+  Optional[Stdlib::AbsolutePath]    $defaults_moduledir       = undef,
+  Optional[Stdlib::AbsolutePath]    $defaults_skeleton        = undef,
+  Stdlib::AbsolutePath              $import_login_defs        = '/etc/login.defs',
+  Stdlib::AbsolutePath              $import_default_useradd   = '/etc/default/useradd',
+  String                            $userdefaults             = "LU_USERNAME = %n\nLU_GIDNUMBER = %u",
+  String                            $groupdefaults            = 'LU_GROUPNAME = %n',
+  Optional[Stdlib::AbsolutePath]    $files_directory          = undef,
+  Optional[Boolean]                 $files_nonroot            = undef,
+  Optional[Stdlib::AbsolutePath]    $shadow_directory         = undef,
+  Optional[Boolean]                 $shadow_nonroot           = undef,
+  Optional[String]                  $ldap_userbranch          = undef,
+  Optional[String]                  $ldap_groupbranch         = undef,
+  Optional[String]                  $ldap_server              = undef,
+  Optional[String]                  $ldap_basedn              = undef,
+  Optional[String]                  $ldap_binddn              = undef,
+  Optional[String]                  $ldap_user                = undef,
+  Optional[String]                  $ldap_password            = undef,
+  Optional[String]                  $ldap_authuser            = undef,
+  Optional[String]                  $ldap_bindtype            = undef,
+  Optional[String]                  $sasl_appname             = undef,
+  Optional[String]                  $sasl_domain              = undef
 ) {
+
+  if ($defaults_hash_rounds_min and $defaults_hash_rounds_max) {
+    if ($defaults_hash_rounds_min >= $defaults_hash_rounds_max) {
+      fail('$defaults_hash_rounds_min must be less than $defaults_hash_rounds_max')
+    }
+  }
 
   file { '/etc/libuser.conf':
     owner   => 'root',
