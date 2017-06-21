280438afebe92931d99d1c5a388b6fb3cf0af63f
brandonrdn <brandon.riden@onyxpoint.com>
2017-06-21 16:42:30 -0400

    (SIMP-3319) Allow users to specify colors as strings (#10)
    Allow users to specify colors as strings instead of ANSI codes. Updated
    sysconfig_init.pp to reflect changes and add comments. The template for
    the sysconfig/init file has been updated to convert these color strings
    to the correct ANSI code.
    
    SIMP-3319 #close

--- manifests/sysconfig_init.pp
+++ manifests/sysconfig_init.pp
@@ -6,6 +6,9 @@
 # @param bootup
 # @param res_col
 # @param move_to_col
+#   * By default, undef will add the code `"echo -en \\033[${RES_COL}G"` to
+#     /etc/sysconfig/init
+#   * Optional string of code will be substituted if included
 # @param setcolor_success
 # @param setcolor_failure
 # @param setcolor_warning
@@ -15,14 +18,27 @@
 # @param prompt
 # @param autoswap
 #
+#For all `setcolor` variables, use the following color options as a string:
+#   * default
+#   * black
+#   * red
+#   * green
+#   * yellow
+#   * blue
+#   * magenta
+#   * cyan
+#   * white
+#
+# author: SIMP Team <simp@simp-project.com>
+#
 class useradd::sysconfig_init (
   Useradd::Bootup      $bootup            = 'color',
   Integer              $res_col           = 60,
-  String               $move_to_col       = "\"echo -en \\\\033[\${RES_COL}G\"",
-  String               $setcolor_success  = "\"echo -en \\\\033[0;32m\"",
-  String               $setcolor_failure  = "\"echo -en \\\\033[0;31m\"",
-  String               $setcolor_warning  = "\"echo -en \\\\033[0;33m\"",
-  String               $setcolor_normal   = "\"echo -en \\\\033[0;39m\"",
+  Optional[String]     $move_to_col       = undef,
+  String               $setcolor_success  = 'green',
+  String               $setcolor_failure  = 'red',
+  String               $setcolor_warning  = 'yellow',
+  String               $setcolor_normal   = 'default',
   Stdlib::AbsolutePath $single_user_login = '/sbin/sulogin',
   Integer[1,8]         $loglvl            = 3,
   Boolean              $prompt            = false,
--- spec/classes/sysconfig_init_spec.rb
+++ spec/classes/sysconfig_init_spec.rb
@@ -9,19 +9,22 @@ describe 'useradd::sysconfig_init' do
         end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_file('/etc/sysconfig/init').with_content(<<-EOM.gsub(/^\s+/,'')
-            BOOTUP=color
-            RES_COL=60
-            MOVE_TO_COL="echo -en \\\\033[${RES_COL}G"
-            SETCOLOR_SUCCESS="echo -en \\\\033[0;32m"
-            SETCOLOR_FAILURE="echo -en \\\\033[0;31m"
-            SETCOLOR_WARNING="echo -en \\\\033[0;33m"
-            SETCOLOR_NORMAL="echo -en \\\\033[0;39m"
-            SINGLE=/sbin/sulogin
-            LOGLEVEL=3
-            PROMPT=no
-            AUTOSWAP=no
-            EOM
+        it { is_expected.to contain_file('/etc/sysconfig/init').with_content(<<-EOF
+# This file managed by Puppet.
+
+
+BOOTUP=color
+RES_COL=60
+MOVE_TO_COL="echo -en \\\\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\\\033[0;32m"
+SETCOLOR_FAILURE="echo -en \\\\033[0;31m"
+SETCOLOR_WARNING="echo -en \\\\033[0;33m"
+SETCOLOR_NORMAL="echo -en \\\\033[0;39m"
+SINGLE=/sbin/sulogin
+LOGLEVEL=3
+PROMPT=no
+AUTOSWAP=no
+            EOF
           )
         }
       end
